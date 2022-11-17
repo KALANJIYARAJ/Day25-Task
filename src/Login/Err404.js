dc185fbc9d902047cb7624c3e5bb77e9e9631bb1
kalanjiyaraj01@gmail.com <kalanjiyaraj01@gmail.com>
2022-11-17 21:03:11 +0530

    Error page button change

--- src/Login/Err404.js
+++ src/Login/Err404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Err404() {
   return (
@@ -7,7 +8,7 @@ function Err404() {
         <div class="error mx-auto" data-text="404">404</div>
         <p class="lead text-gray-800 mb-5">Page Not Found</p>
         <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-        <a href="index.html">&larr; Back to Dashboard</a>
+        <Link to={"/portal/dashboard"}>&larr; Back to Dashboard</Link>
     </div>
     </div>
   )
